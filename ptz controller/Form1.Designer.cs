eb701e10319fe3fba3b8efe5392c2b9e035951e4
fairdogJR <tfairfie@gmail.com>
2020-07-30 23:58:20 -0400

    includes code for positional feedback of servos from Arduino side by reading serial and arduino code for fixed positions, soon to be inked to arduino

--- ptz controller/Form1.Designer.cs
+++ ptz controller/Form1.Designer.cs
@@ -37,7 +37,19 @@
             this.Up = new System.Windows.Forms.Button();
             this.down = new System.Windows.Forms.Button();
             this.rcvbuf = new System.Windows.Forms.Label();
-            this.right_position = new System.Windows.Forms.Label();
+            this.x_position = new System.Windows.Forms.Label();
+            this.store1 = new System.Windows.Forms.Button();
+            this.store2 = new System.Windows.Forms.Button();
+            this.store3 = new System.Windows.Forms.Button();
+            this.store4 = new System.Windows.Forms.Button();
+            this.recall4 = new System.Windows.Forms.Button();
+            this.recall3 = new System.Windows.Forms.Button();
+            this.recall2 = new System.Windows.Forms.Button();
+            this.recall1 = new System.Windows.Forms.Button();
+            this.fave1 = new System.Windows.Forms.Label();
+            this.fave2 = new System.Windows.Forms.Label();
+            this.fave3 = new System.Windows.Forms.Label();
+            this.fave4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -125,27 +137,153 @@
             this.rcvbuf.AutoSize = true;
             this.rcvbuf.Location = new System.Drawing.Point(1017, 332);
             this.rcvbuf.Name = "rcvbuf";
-            this.rcvbuf.Size = new System.Drawing.Size(52, 20);
+            this.rcvbuf.Size = new System.Drawing.Size(48, 20);
             this.rcvbuf.TabIndex = 9;
-            this.rcvbuf.Text = "rcvbuf";
+            this.rcvbuf.Text = "0 0 0 ";
             this.rcvbuf.Click += new System.EventHandler(this.rcvbuf_Click);
             // 
-            // right_position
+            // x_position
             // 
-            this.right_position.AutoSize = true;
-            this.right_position.Location = new System.Drawing.Point(758, 385);
-            this.right_position.Name = "right_position";
-            this.right_position.Size = new System.Drawing.Size(104, 20);
-            this.right_position.TabIndex = 10;
-            this.right_position.Text = "right_position";
-            this.right_position.Click += new System.EventHandler(this.label1_Click_1);
+            this.x_position.AutoSize = true;
+            this.x_position.Location = new System.Drawing.Point(758, 385);
+            this.x_position.Name = "x_position";
+            this.x_position.Size = new System.Drawing.Size(80, 20);
+            this.x_position.TabIndex = 10;
+            this.x_position.Text = "x_position";
+            this.x_position.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // store1
+            // 
+            this.store1.Location = new System.Drawing.Point(1108, 297);
+            this.store1.Name = "store1";
+            this.store1.Size = new System.Drawing.Size(74, 38);
+            this.store1.TabIndex = 11;
+            this.store1.Text = "store1";
+            this.store1.UseVisualStyleBackColor = true;
+            this.store1.Click += new System.EventHandler(this.store1_Click);
+            // 
+            // store2
+            // 
+            this.store2.Location = new System.Drawing.Point(1108, 341);
+            this.store2.Name = "store2";
+            this.store2.Size = new System.Drawing.Size(74, 38);
+            this.store2.TabIndex = 13;
+            this.store2.Text = "store2";
+            this.store2.UseVisualStyleBackColor = true;
+            this.store2.Click += new System.EventHandler(this.store2_Click);
+            // 
+            // store3
+            // 
+            this.store3.Location = new System.Drawing.Point(1108, 385);
+            this.store3.Name = "store3";
+            this.store3.Size = new System.Drawing.Size(74, 38);
+            this.store3.TabIndex = 14;
+            this.store3.Text = "store3";
+            this.store3.UseVisualStyleBackColor = true;
+            this.store3.Click += new System.EventHandler(this.store3_Click);
+            // 
+            // store4
+            // 
+            this.store4.Location = new System.Drawing.Point(1108, 429);
+            this.store4.Name = "store4";
+            this.store4.Size = new System.Drawing.Size(74, 38);
+            this.store4.TabIndex = 15;
+            this.store4.Text = "store4";
+            this.store4.UseVisualStyleBackColor = true;
+            this.store4.Click += new System.EventHandler(this.store4_Click);
+            // 
+            // recall4
+            // 
+            this.recall4.Location = new System.Drawing.Point(1308, 429);
+            this.recall4.Name = "recall4";
+            this.recall4.Size = new System.Drawing.Size(74, 38);
+            this.recall4.TabIndex = 19;
+            this.recall4.Text = "recall4";
+            this.recall4.UseVisualStyleBackColor = true;
+            // 
+            // recall3
+            // 
+            this.recall3.Location = new System.Drawing.Point(1308, 385);
+            this.recall3.Name = "recall3";
+            this.recall3.Size = new System.Drawing.Size(74, 38);
+            this.recall3.TabIndex = 18;
+            this.recall3.Text = "recall3";
+            this.recall3.UseVisualStyleBackColor = true;
+            // 
+            // recall2
+            // 
+            this.recall2.Location = new System.Drawing.Point(1308, 341);
+            this.recall2.Name = "recall2";
+            this.recall2.Size = new System.Drawing.Size(74, 38);
+            this.recall2.TabIndex = 17;
+            this.recall2.Text = "recall2";
+            this.recall2.UseVisualStyleBackColor = true;
+            // 
+            // recall1
+            // 
+            this.recall1.Location = new System.Drawing.Point(1308, 297);
+            this.recall1.Name = "recall1";
+            this.recall1.Size = new System.Drawing.Size(74, 38);
+            this.recall1.TabIndex = 16;
+            this.recall1.Text = "recall1";
+            this.recall1.UseVisualStyleBackColor = true;
+            // 
+            // fave1
+            // 
+            this.fave1.AutoSize = true;
+            this.fave1.Location = new System.Drawing.Point(1216, 306);
+            this.fave1.Name = "fave1";
+            this.fave1.Size = new System.Drawing.Size(44, 20);
+            this.fave1.TabIndex = 20;
+            this.fave1.Text = "0 0 0";
+            this.fave1.Click += new System.EventHandler(this.fave1_Click_2);
+            // 
+            // fave2
+            // 
+            this.fave2.AutoSize = true;
+            this.fave2.Location = new System.Drawing.Point(1216, 350);
+            this.fave2.Name = "fave2";
+            this.fave2.Size = new System.Drawing.Size(44, 20);
+            this.fave2.TabIndex = 21;
+            this.fave2.Text = "0 0 0";
+            this.fave2.Click += new System.EventHandler(this.fave2_Click);
+            // 
+            // fave3
+            // 
+            this.fave3.AutoSize = true;
+            this.fave3.Location = new System.Drawing.Point(1216, 394);
+            this.fave3.Name = "fave3";
+            this.fave3.Size = new System.Drawing.Size(44, 20);
+            this.fave3.TabIndex = 22;
+            this.fave3.Text = "0 0 0";
+            // 
+            // fave4
+            // 
+            this.fave4.AutoSize = true;
+            this.fave4.Location = new System.Drawing.Point(1216, 438);
+            this.fave4.Name = "fave4";
+            this.fave4.Size = new System.Drawing.Size(44, 20);
+            this.fave4.TabIndex = 23;
+            this.fave4.Text = "0 0 0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1428, 762);
-            this.Controls.Add(this.right_position);
+            this.Controls.Add(this.fave4);
+            this.Controls.Add(this.fave3);
+            this.Controls.Add(this.fave2);
+            this.Controls.Add(this.fave1);
+            this.Controls.Add(this.recall4);
+            this.Controls.Add(this.recall3);
+            this.Controls.Add(this.recall2);
+            this.Controls.Add(this.recall1);
+            this.Controls.Add(this.store4);
+            this.Controls.Add(this.store3);
+            this.Controls.Add(this.store2);
+            this.Controls.Add(this.store1);
+            this.Controls.Add(this.x_position);
             this.Controls.Add(this.rcvbuf);
             this.Controls.Add(this.down);
             this.Controls.Add(this.Up);
@@ -175,7 +313,19 @@
         private System.Windows.Forms.Button Up;
         private System.Windows.Forms.Button down;
         private System.Windows.Forms.Label rcvbuf;
-        private System.Windows.Forms.Label right_position;
+        private System.Windows.Forms.Label x_position;
+        private System.Windows.Forms.Button store1;
+        private System.Windows.Forms.Button store2;
+        private System.Windows.Forms.Button store3;
+        private System.Windows.Forms.Button store4;
+        private System.Windows.Forms.Button recall4;
+        private System.Windows.Forms.Button recall3;
+        private System.Windows.Forms.Button recall2;
+        private System.Windows.Forms.Button recall1;
+        private System.Windows.Forms.Label fave1;
+        private System.Windows.Forms.Label fave2;
+        private System.Windows.Forms.Label fave3;
+        private System.Windows.Forms.Label fave4;
     }
 }
 
